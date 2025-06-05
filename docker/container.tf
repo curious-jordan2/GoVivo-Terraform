b69cd8620aa036292833a02abc04a3d219e26def
curious-jordan2 <jordan.breiner@govivosolutions.com>
2025-06-05 14:25:48 -0500

    updated conatiner.tf to be dynamic based on os

--- docker/container.tf
+++ docker/container.tf
@@ -1,7 +1,13 @@
+# Creat locals
+locals {
+    network_mode = var.local_os == "windows" ? "nat" : "bridge"
+}
+
 # Create a container
 resource "docker_container" "container" {
     image = docker_image.image.image_id
     name  = var.container_name
+    network_mode = local.network_mode
     ports {
         internal = 80
         external = var.external_port
